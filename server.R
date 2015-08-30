bd684e60b691ba036f0377973cd80ae535e4df15
vioxx <shomenuchi@gmail.com>
2015-08-29 19:23:57 -0500

    better code

--- server.R
+++ server.R
@@ -12,19 +12,10 @@ shinyServer(function(input, output) {
     
     a <- read.csv(inFile$datapath, header=input$header, sep=input$sep, 
                       quote=input$quote)
-    
-    M1 <- input$Method1
-    M2 <- input$Method2
-    
-    colnames(a)[1] <- M1
-    colnames(a)[2] <- M2
-    
-    M1 <- input$Method1
-    M2 <- input$Method2
-    plot(x=a$M1,y=a$M2)
-#    ggplot(a, aes(x=(a$M1+a$M2)/2, y=(a$M1-a$M2)/2)) + 
-#    geom_point() +
-#    labs(x='Method Mean',y='Method Difference')
+
+    ggplot(a, aes_string(colnames(a)[1],y=colnames(a)[2])) +
+    geom_point() +
+    labs(x='Method Mean',y='Method Difference')
   })
   
   output$summary <- renderPrint({
@@ -36,8 +27,8 @@ shinyServer(function(input, output) {
     
     b <- read.csv(inFile$datapath, header=input$header, sep=input$sep, 
                   quote=input$quote)   
-    
-    bland.altman.stats(b$Method1, b$Method2, 
+
+    bland.altman.stats(b$b1, b$b2, 
                        two = 1.96, mode = 2, conf.int = 0.95)
   })
   
@@ -50,12 +41,6 @@ shinyServer(function(input, output) {
     
     read.csv(inFile$datapath, header=input$header, sep=input$sep, 
              quote=input$quote)
-  })
-  
-  output$Data <- renderText({
-    
-    paste(input$Method1, "vs.", input$Method2)
-    
   })
   
 })
